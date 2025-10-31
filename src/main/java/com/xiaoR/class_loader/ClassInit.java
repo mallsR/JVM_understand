bd868b499333dee8e1884ff0a401b71559594724
xiaoR <2471385107@qq.com>
2025-10-31 14:40:22 +0800

    feat(class_loader):类初始化过程演示代码
    - 新增 ClassInit 类用于演示类加载器的初始化过程
    - 添加 Lombok 和 SLF4J 依赖以支持日志记录
    - 实现静态块和静态变量初始化逻辑
    - 演示 <clinit> 和 <init> 方法的生成与执行顺序
    - 添加日志输出以观察类初始化阶段的行为
    - 更新 pom.xml 添加 lombok、slf4j 和 logback 依赖项
    - 移除 Main 类中的默认 Hello World 输出内容

--- src/main/java/com/xiaoR/class_loader/ClassInit.java
+++ src/main/java/com/xiaoR/class_loader/ClassInit.java
@@ -1,10 +1,45 @@
 package com.xiaoR.class_loader;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xiaoR
  * @version 1.0
  * @date 2025/10/31
- * @description 
+ * @description 演示类加载器中Initialization过程的原理
  */
+@Slf4j
 public class ClassInit {
+    /**
+     * 一个必定有一个构造方法, 所以其对应的字节码文件的方法中一定拥有一个<init>方法
+     */
+    private int num1 = 10;
+    public ClassInit() {
+        num1 = 20;
+        int num2 = 20;
+    }
+
+    /**
+     * 类中至少定义了一个静态变量, 才会在类对应的字节码文件中, 生成一个<clinit>方法
+     */
+    private static int s_num = 1;
+
+    static {
+        log.info("ClassInit static block");
+        s_num = 2;
+        s_number = 20;
+        log.info("ClassInit static block num: {}", s_num);
+//        log.info("ClassInit static block number: {}", number);  // 报错: 非法的前向引用
+    }
+
+    /**
+     * 在类加载器的Linking阶段中, prepare步骤会自动将静态变量number = 0,
+     * 然后在initial阶段, 先将number = 20, 然后再将number = 10
+     */
+    private static int s_number = 10;
+
+    public static void main(String[] args) {
+        log.info("ClassInit main num: {}", s_num);
+        log.info("ClassInit main number: {}", s_number);
+    }
 }
